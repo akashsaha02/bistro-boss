10f130c020918e1f57a9e031d022297871c560a0
Akash4056 <asaha8313@gmail.com>
2025-01-06 15:34:21 +0530

    Integrate MongoDB for menu data retrieval; update API endpoint and refactor useMenu hook for dynamic fetching

--- client/src/hooks/useMenu.jsx
+++ client/src/hooks/useMenu.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
 const useMenu = () => {
     const [menu, setMenu] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get('/menu.json')
+        axios.get(`${apiBaseUrl}/menu`)
             .then((res) => {
                 setMenu(res.data);
                 setLoading(false);
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middlewares
@@ -9,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,12 +25,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database=client.db("bistroDb");
+    const menuCollection=database.collection("menu");
+
+    app.get('/menu', async (req, res) => {
+        const menu = await menuCollection.find().toArray();
+        res.send(menu);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
